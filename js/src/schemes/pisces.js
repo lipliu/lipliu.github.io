5f5c35a69f9d31bd12cbed71792ba35c00d32e77
lipliu <cheng994@vip.qq.com>
2019-12-21 13:32:15 +0800

    Site updated: 2019-12-21 13:32:11

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,17 +1,3 @@
-$(document).ready(function () {
-  var $headerInner = $('.header-inner');
-  var $sidebar = $('#sidebar');
-  var getSidebarTop = function(){
-    return $headerInner.height() + CONFIG.sidebar.offset;
-  };
-  var setSidebarMarginTop = function(sidebarTop){
-    return $sidebar.css({ 'margin-top': sidebarTop });
-  };
-  var mql = window.matchMedia('(min-width: 991px)');
-  setSidebarMarginTop(getSidebarTop()).show();
-  mql.addListener(function(e){
-    if(e.matches){
-      setSidebarMarginTop(getSidebarTop());
-    }
-  });
-});
+// build time:Sat Dec 21 2019 13:32:02 GMT+0800 (GMT+08:00)
+$(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
+//rebuild by neat 
