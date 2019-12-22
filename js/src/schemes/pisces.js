6af9eac6ed7cee18ab0944a7dbf9457954dcf078
lipliu <cheng994@vip.qq.com>
2019-12-22 10:26:37 +0800

    Site updated: 2019-12-22 10:26:32

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Sat Dec 21 2019 22:39:41 GMT+0800 (GMT+08:00)
+// build time:Sun Dec 22 2019 10:26:24 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
